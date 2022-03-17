784d12b4e43c9ab6c59336a0b7808daeed1df42d
onishi.kohei <kohei.o2222@gmail.com>
2022-03-17 22:41:23 +0900

    cargoへのpackage追加
    cargo install --locked trunk
    cargo install wasm-bindgen-cli

--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -3,7 +3,11 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <h1>{ "Hello World" }</h1>
+        <div>
+            <h1>{ "Hello World" }</h1>
+        </div>
+
+
     }
 }
 

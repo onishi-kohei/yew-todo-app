25cb111c09371d20d4bc7496f3bd39a19ee7a73a
onishi.kohei <kohei.o2222@gmail.com>
2022-03-17 22:50:15 +0900

    Headerコンポーネント作成

--- yew-app/src/main.rs
+++ yew-app/src/main.rs
@@ -1,9 +1,12 @@
 use yew::prelude::*;
+use components::header::Header;
 
+mod components;
 #[function_component(App)]
 fn app() -> Html {
     html! {
         <div>
+            <Header />
             <h1>{ "Hello World" }</h1>
         </div>
 
